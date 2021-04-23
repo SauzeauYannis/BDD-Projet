d1651aef1837751ba3f3f3fc886afbaeaf4b2de1
404Monkey <60445154+404Monkey@users.noreply.github.com>
2021-04-23 17:40:56 +0200

    create requete.sql

---
+++ 6_Requete/requete.sql
@@ -0,0 +1,80 @@
+-- Requete
+
+-- TODO Requete 1
+-- 1
+
+
+-- TODO Requete 2
+-- 2
+
+
+-- TODO Requete 3
+-- 3
+
+
+-- TODO Requete 4
+-- 4
+
+
+-- TODO Requete 5
+-- 5
+
+
+-- TODO Requete 6
+-- 6
+
+
+-- TODO Requete 7
+-- 7
+
+
+-- TODO Requete 8
+-- 8
+
+
+-- TODO Requete 9
+-- 9
+
+
+-- TODO Requete 10
+-- 10
+
+
+-- TODO Requete 11
+-- 11
+
+
+-- TODO Requete 12
+-- 12
+
+
+-- TODO Requete 13
+-- 13
+
+
+-- TODO Requete 14
+-- 14
+
+
+-- TODO Requete 15
+-- 15
+
+
+-- TODO Requete 16
+-- 16
+
+
+-- TODO Requete 17
+-- 17
+
+
+-- TODO Requete 18
+-- 18
+
+
+-- TODO Requete 19
+-- 19
+
+
+-- TODO Requete 20
+-- 20
