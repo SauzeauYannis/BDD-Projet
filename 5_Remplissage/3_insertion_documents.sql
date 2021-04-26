0601618eece7052260095c1fa9f9c72e57bb592c
404Monkey <60445154+404Monkey@users.noreply.github.com>
2021-04-26 10:50:05 +0200

    add some datas

--- 5_Remplissage/3_insertion_documents.sql
+++ 5_Remplissage/3_insertion_documents.sql
@@ -4,7 +4,16 @@
 -- Thèmes
 
 INSERT INTO Theme (word)
-VALUES ('Fantastic');
+VALUES ('Fantastique');
+
+INSERT INTO Theme (word)
+VALUES ('Informatique');
+
+INSERT INTO Theme (word)
+VALUES ('Mathématique');
+
+INSERT INTO Theme (word)
+VALUES ('Polar');
 
 
 -- Mot clefs
@@ -14,6 +23,27 @@ VALUES ('Magic');
 
 INSERT INTO Keyword (word)
 VALUES ('School');
+
+INSERT INTO Keyword (word)
+VALUES ('C');
+
+INSERT INTO Keyword (word)
+VALUES ('C++');
+
+INSERT INTO Keyword (word)
+VALUES ('Python');
+
+INSERT INTO Keyword (word)
+VALUES ('SQL');
+
+INSERT INTO Keyword (word)
+VALUES ('Java');
+
+INSERT INTO Keyword (word)
+VALUES ('Programmation');
+
+INSERT INTO Keyword (word)
+VALUES ('Base de données');
 
 
 -- Documents
@@ -28,13 +58,82 @@ INSERT INTO Document (title, document_category_id, publisher_id, theme_id)
 VALUES ('Blanche fesses et les sept mains', 3, 1, 1);
 
 INSERT INTO Document (title, document_category_id, publisher_id, theme_id)
-VALUES ('stepsis is stuck', 4, 1, 1);
+VALUES ('stepsis is stuck', 1, 1, 1);
+
+INSERT INTO Document (title, document_category_id, publisher_id, theme_id)
+VALUES ('SQL pour les nuls', 1, 5, 2);
+
+INSERT INTO Document (title, document_category_id, publisher_id, theme_id)
+VALUES ('Apprendre le C', 1, 3, 2);
+
+INSERT INTO Document (title, document_category_id, publisher_id, theme_id)
+VALUES ('Cours et exercices C', 1, 2, 2);
+
+INSERT INTO Document (title, document_category_id, publisher_id, theme_id)
+VALUES ('Apprendre le C++', 1, 3, 2);
+
+INSERT INTO Document (title, document_category_id, publisher_id, theme_id)
+VALUES ('Cours et exercices C++', 1, 2, 2);
+
+INSERT INTO Document (title, document_category_id, publisher_id, theme_id)
+VALUES ('Apprendre le Java', 1, 3, 2);
+
+INSERT INTO Document (title, document_category_id, publisher_id, theme_id)
+VALUES ('Cours et exercices Java', 1, 2, 2);
+
+INSERT INTO Document (title, document_category_id, publisher_id, theme_id)
+VALUES ('Apprendre le Python', 1, 3, 2);
+
+INSERT INTO Document (title, document_category_id, publisher_id, theme_id)
+VALUES ('Cours et exercices Python', 1, 2, 2);
 
 
 -- Autheur(s) d'un document
 
 INSERT INTO Document_author (document_id, author_id)
 VALUES (1, 1);
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (5, 2);
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (6, 3);
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (8, 3);
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (10, 3);
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (12, 3);
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (7, 4);;
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (9, 4);;
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (11, 4);;
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (13, 4);;
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (6, 5);
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (8, 5);
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (8, 5);
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (11, 6);
+
+INSERT INTO Document_author (document_id, author_id)
+VALUES (13, 6);
 
 
 -- Mot clef(s) d'un document
@@ -45,11 +144,20 @@ VALUES (1, 1);
 INSERT INTO Document_keyword (document_id, keyword_id)
 VALUES (1, 2);
 
+INSERT INTO Document_keyword (document_id, keyword_id)
+VALUES (5, 6);
+
+INSERT INTO Document_keyword (document_id, keyword_id)
+VALUES (5, 9);
+
 
 -- Livres
 
 INSERT INTO Book (document_id, page_number)
 VALUES (1, 500);
+
+INSERT INTO Book (document_id, page_number)
+VALUES (5, 480);
 
 
 -- CD
@@ -83,3 +191,6 @@ VALUES (3, 1, 100);
 
 INSERT INTO Copy (copy_id, document_id, shelf_number)
 VALUES (4, 1, 100);
+
+INSERT INTO Copy (copy_id, document_id, shelf_number)
+VALUES (1, 5, 200);
